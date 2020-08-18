2dba5d05aae5f5ae015b0119ef1936e54e2849f6
khinsandiaung <sandiaungxeno25>
2020-08-18 19:59:21 +0630

    show order detail

--- resources/views/backend/orders/show.blade.php
+++ resources/views/backend/orders/show.blade.php
@@ -1,38 +1,70 @@
 @extends('backendtemplate')
 @section('content')
 <div class="container-fluid">
-	<h2>Order Detail (Your UI)</h2>
-	<div class="container-fluid">
-		
-			<div class="row">
-				
-				<div class="col-md-6">
-					<label> Item Name</label>
-				</div>
-				<div class="col-md-6">
-					<p>{{$order->item->name}}</p>
-				</div>
-				<div class="col-md-6">
-					<label> Price</label>
-				</div>
-				<div class="col-md-6">
-					{{-- <p>{{$order->item->price}}</p> --}}
-				</div>
-				<div class="col-md-6">
-					<label> Qty</label>
-				</div>
-				<div class="col-md-6">
-					{{-- <p>{{$order->orderdetail->qty}}</p> --}}
-				</div>
-				<div class="col-md-6">
-					<label> Total</label>
-				</div>
-				<div class="col-md-6">
-					{{-- <p>{{$order->total}}</p> --}}
-				</div>
-
-			</div>
-	</div>
-	</div>
+	<h2>Order Detail</h2>
 	
-	@endsection
+		<div class="row">
+			<div class="offset-2 col-md-8">
+				<table class="table table-bordered">
+				<tbody>
+			<tr>
+				<td class="font-weight-bold text-center" colspan="4">
+					<p>Xeno Shop</p>
+					<p>Mandalay</p>
+					<p>Tel: 092345678</p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="4">
+					<div class="row">
+
+						<div class="col-md-3 font-weight-bold">Casher<br><br>Voucher</div>
+						<div class="col-md-3">
+							{{Auth::user()->name}}
+							<br>
+							<br>
+							{{$order->voucherno}}
+						</div>
+						<div class="col-md-3 font-weight-bold">Date<br><br>Order Time</div>
+						<div class="col-md-3">
+							<?php $d=date('Y-m-d');
+							echo $d; ?>
+							<br>
+							<br>
+							{{$order->created_at}}
+						</div>
+					</div>
+					
+				</td>
+			</tr>
+			<tr>
+				<th>Item Name</th>
+				<th>Price</th>
+				<th>Qty</th>
+				<th>Amount</th>
+
+			</tr>
+			@foreach($order->items as $order1)
+			<tr>
+				<td>{{$order1->name}}</td>
+				<td>{{$order1->price}}</td>
+				<td>{{$order1->pivot->qty}}</td>
+				<td>{{$order1->pivot->qty*$order1->price}}</td>
+			</tr>
+			@endforeach
+
+			<tr>
+				<td colspan="3">Total Amount</td>
+				<td>{{$order->total}}</td>
+			</tr>
+		</tbody>
+		</table>
+			</div>
+		</div>
+
+
+	
+
+</div>
+
+@endsection
